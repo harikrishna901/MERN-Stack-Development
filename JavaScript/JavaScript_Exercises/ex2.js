0d83fd18164e1931fa5714c64bfd147a9a7ccb5e
Golla Harikrishna <gollaharikrishna6381@gmail.com>
2026-09-06 21:18:33 +0530

    Delete JavaScript directory

--- JavaScript/JavaScript_Exercises/ex2.js
+++
@@ -1,6 +0,0 @@
-const arr = [1,2,3,4,5];
-//arr.forEach((element,index,array)=>{element%2===0?console.log(element):""});
-const newarr = arr.filter((element,index,array) =>element);
-const value = arr.reduce((previous,element)=> Math.max(previous,element));
-console.log(value);
-console.log(newarr);
